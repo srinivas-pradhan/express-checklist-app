2c71945d0d1695e6a512f9978f1d41bf8c3e961c
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-02-23 17:24:15 -0500

    Initial app setup

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Yeah')
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res) => {
+    console.log({data: "Hello World"})
+    return res.send('Hello')
+})
+
+
+app.listen(5000, () => {
+    console.log(`App is lisening on Port 5000 ...`)
+})
